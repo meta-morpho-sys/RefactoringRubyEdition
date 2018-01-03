b1978abf0cdf26aaa3f459c254a269d3d5a97423
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-01-03 20:26:10 +0000

    Refactoring of frequent_renter_points feature. Used Extract Module technique.

--- lib/pricers.rb
+++ lib/pricers.rb
@@ -1,5 +1,15 @@
+# The module returns number f points for all rents that are not
+# a New Release rent.
+module RentersPoints
+  def frequent_renter_points
+    1
+  end
+end
+
 # Holds type code behaviour
 class RegularPrice
+  include RentersPoints
+
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -12,10 +22,15 @@ class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
 
 # Holds type code behaviour
 class ChildrensPrice
+  include RentersPoints
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
--- spec/pricers_spec.rb
+++ spec/pricers_spec.rb
@@ -1,34 +1,49 @@
 require 'pricers'
 
 describe RegularPrice do
+  let(:regular) { RegularPrice.new }
+
   it 'calculates the charge with rent-days' do
-    regular = RegularPrice.new
     expect(regular.charge(1)).to eq 2
   end
 
   it 'calculates the charge with more than 2 days' do
-    regular = RegularPrice.new
     expect(regular.charge(3)).to eq 3.5
+  end
+
+  it 'calculates points for frequent use' do
+    expect(regular.frequent_renter_points).to eq 1
   end
 end
 
 describe NewReleasePrice do
+  let(:new_release) { NewReleasePrice.new }
+
   it 'calculates the charge with rent-days' do
-    new_release = NewReleasePrice.new
     expect(new_release.charge(1)).to eq 3
+  end
+
+  it 'calculates regular points for frequent use' do
+    expect(new_release.frequent_renter_points(1)).to eq 1
+  end
+
+  it 'calculates bonus points for frequent use' do
+    expect(new_release.frequent_renter_points(2)).to eq 2
   end
 end
 
 describe ChildrensPrice do
-  it 'calculates the charge with rent-days' do
-    kids = ChildrensPrice.new
+  let(:kids) { ChildrensPrice.new }
+
+  it 'calculates the charge with 1 rent-day' do
     expect(kids.charge(1)).to eq 1.5
   end
 
   it 'calculates the charge with rent-days' do
-    kids = ChildrensPrice.new
     expect(kids.charge(4)).to eq 3
   end
 
-
+  it 'calculates points for frequent use' do
+    expect(kids.frequent_renter_points).to eq 1
+  end
 end
